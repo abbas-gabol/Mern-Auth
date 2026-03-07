8bffd925b9e4d8f144f8ff4d0f7e2ff3bd63c213
Abbas-gabol <abbasgabol321@gmail.com>
2026-03-07 12:14:42 -0800

    Add registration successful transactional email

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+<<<<<<< Updated upstream
+=======
+import {User} from '../model/user.model.js';
+import { sendEmail } from '../services/email.service.js';
+import { welcomeEmailTemplate } from '../services/email.template.js';
+
+const registerUser = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = await User.create({
+      username: username.toLowerCase().trim(),
+      email: email.toLowerCase().trim(),
+      password,
+    });
+    const { subject, html } = welcomeEmailTemplate(newUser.username);
+    try {
+      await sendEmail({ to: newUser.email, subject, html });
+      console.log("Welcome email sent to:", newUser.email);
+    } catch (emailError) {
+      console.error("Failed to send welcome email:", emailError.message);
+    }
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        userId: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+  });
+ 
+
+} catch (error) {
+  next(error);
+  }
+};
+export {registerUser};
+>>>>>>> Stashed changes
---
+++ src/services/email.service.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' }); 
+
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_SMTP_USER,
+    pass: process.env.BREVO_SMTP_PASS,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Brevo SMTP connection failed:", error.message);
+  } else {
+    console.log("Brevo SMTP is ready ✅");
+  }
+});
+export const sendEmail = async ({ to, subject, html }) => {
+  await transporter.sendMail({
+    from: `"Authcore" <${process.env.SENDER_EMAIL}>`,
+    to,
+    subject,
+    html,
+  });
+};
