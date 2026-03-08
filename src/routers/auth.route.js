bee5b1f2be7599a9f0d250a8d8bfe5b06ff170dd
Abbas-gabol <abbasgabol321@gmail.com>
2026-03-08 14:20:51 -0800

    remove accidentally added nested repository

--- src/routers/auth.route.js
+++ src/routers/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {
+  registerUser,
   loginUser,
   refreshAccessToken,
   logoutUser,
@@ -8,7 +9,7 @@ import {
 import { authMiddleware } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
-
+router.post('/register',     registerUser);
 router.post('/login',        loginUser);
 router.post('/refresh',      refreshAccessToken);   
 router.post('/logout',       logoutUser);         
